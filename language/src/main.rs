ebf64c874f9c3eb9ea249a607b3078c6c362bc46
Alec Thilenius <alec@thilenius.com>
2019-02-13 07:24:54 -0700

    Add a custom recursive descent PEG parser, remove LALRPOP.
    Add the macros and most of a macro-based PEG recursive decent parser and
    remove LALRPOP from the repo completely. The parser already works very
    well, although it still needs a few key things, and will likly be turned
    into a Packrat parser later by adding Memoization.

--- language/src/main.rs
+++ language/src/main.rs
@@ -1,27 +1,62 @@
 #[macro_use]
-extern crate lalrpop_util;
-extern crate clap;
+extern crate lazy_static;
+
+// extern crate clap;
 extern crate regex;
 
-mod ast;
-mod scope;
-mod vm;
+// mod ast;
+#[macro_use]
+mod parser;
+mod grammar;
+// mod scope;
+// mod vm;
 
-use crate::vm::*;
-use clap::{App, Arg};
-use std::fs;
+// use crate::vm::*;
+// use clap::{App, Arg};
+// use std::fs;
+use crate::parser::*;
+
+productions! {
+
+  ident -> &'a str {
+    { name:[r"[a-zA-Z][a-zA-Z0-9_-]*"] } => name
+  }
+
+  statement -> String {
+    { _:[r"let"] ident:[ident] _:[r"="] expr:[expression] _:[r";"] } => format!("let {} = {};", ident, expr)
+  }
+
+  expression -> String {
+    { ident:[ident] } => ident.to_owned(),
+    { _:[r"\{"] expr:[expression] _:[r"\}"] } => expr,
+  }
+
+}
 
 fn main() {
-    let matches = App::new("atc")
-        .arg(
-            Arg::with_name("input_file")
-                .required(true)
-                .takes_value(true),
-        )
-        .get_matches();
-    let filename = matches.value_of("input_file").unwrap();
-    let contents = fs::read_to_string(filename).expect("Cannot read file");
+  // let matches = App::new("atc")
+  //     .arg(
+  //         Arg::with_name("input_file")
+  //             .required(true)
+  //             .takes_value(true),
+  //     )
+  //     .get_matches();
+  // let filename = matches.value_of("input_file").unwrap();
+  // let contents = fs::read_to_string(filename).expect("Cannot read file");
 
-    let mut vm = VM::new();
-    vm.exec(&contents);
+  // let mut vm = VM::new();
+  // vm.exec(&contents);
+
+  // let sample = r#"
+  //     "Hello, \"world!" "And another!"
+  // "#;
+  // let parser = Parser::new(sample);
+  // let tokens = parser.parse();
+  // println!("{:#?}", tokens);
+
+  let source = r"let foo_bar = { baz_bang };";
+  let mut parser = Parser::new();
+  if let Some(res) = parser.parse_or_log_errors(statement, source) {
+    println!("{:#?}", res);
+  }
 }
